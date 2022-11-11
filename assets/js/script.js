b97abf32693536c2d580f15c19f55d627111a433
Ruan Figuereido <huanfiguereido@gmail.com>
2022-11-11 18:01:24 -0300

    inclusão de JS
    adicionei o javascript e as interatividades com os dois botões de curriculo e sobre mim.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+let botao=document.getElementById('btn');
+let mais=document.getElementById('maissobremim');
+
+function SobreMim(){
+    mais.innerHTML=`Eu comecei a estudar programação para ver como era á area
+    e caso eu gostasse dela eu seguiria estudando. isso foi há mais de um ano atrás
+    e hoje estou aqui escrevendo sobre mim no meu portfólio que EU mesmo criei
+    através de tudo que aprendi nessa pequena jornada. Nasci na cidade de Aracaju-SE
+    endereço este ao qual resido atualmente e busco uma oportunidade de ingressar
+    na área de tecnologia de forma profissional exercendo com segurança e confiança
+    todos os meus aprendizados nas tecnologias HTML5, CSS3, JavaSript e GIT/GitHub.
+    além de estar estudando Bootstrap,Node.js e futuramente React.js. Venho por meio
+    deste portólio exibir alguns dos meus projetos onde este portólio também foi
+    criado por mim, tornando-o também um projeto meu.`;
+    mais.style.fontSize='1.5em';
+    botao.style.display='none';
+}
