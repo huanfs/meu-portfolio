97b6a9a869df53ca89e341b7671d81593054ba46
Ruan Figuereido <huanfiguereido@gmail.com>
2023-03-01 18:47:13 -0300

    ajustes
    ajustes de design, posicionamento, espaçamentos e cores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-let botao=document.getElementById('btn');
-let mais=document.getElementById('maissobremim');
 
 function SobreMim(){
+    let botao=document.getElementById('btn');
+    let mais=document.getElementById('maissobremim');
+    let sobre=document.querySelector("#sobre-mim h2");
     mais.innerHTML=`Eu comecei a estudar programação para ver como era á area
     e caso eu gostasse dela eu seguiria estudando. isso foi há mais de um ano atrás
     e hoje estou aqui escrevendo sobre mim no meu portfólio que EU mesmo criei
@@ -10,8 +11,8 @@ function SobreMim(){
     na área de tecnologia de forma profissional exercendo com segurança e confiança
     todos os meus aprendizados nas tecnologias HTML5, CSS3, JavaSript e GIT/GitHub.
     além de estar estudando Bootstrap,Node.js e futuramente React.js.`;
-    mais.style.fontSize='1.1em';
+    mais.style.fontSize='1.2em';
+    mais.style.marginTop="10px";
     botao.style.display='none';
-
-
+    sobre.style.marginBottom="-30px";
 }
