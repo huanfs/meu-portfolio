cab0e46ca75dc0af2cd565d0e87e49e4e6dbc570
Ruan Figuereido <huanfiguereido@gmail.com>
2023-10-03 17:34:01 -0300

    ajustes de layout, agora a minha foto é exibida corretamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,14 +23,14 @@ function SobreMim(){
     let mais=document.getElementById('maissobremim');
     let sobreMim = document.querySelector("#sobre-mim");
     let eu=document.querySelector("#eu");
-    mais.innerHTML=`Eu comecei a estudar programação para ver como era á area
-    e caso eu gostasse dela eu seguiria estudando. isso foi há mais de um ano atrás
-    e hoje estou aqui escrevendo sobre mim no meu portfólio que EU mesmo criei
+    mais.innerHTML=`Eu comecei a estudar programação para ver como era a área
+    e se caso eu gostasse dela, seguiria estudando. isso foi há mais de um ano,
+    hoje estou aqui escrevendo sobre mim no meu portfólio que EU mesmo criei
     através de tudo que aprendi nessa pequena jornada. Nasci na cidade de Aracaju-SE
     endereço este ao qual resido atualmente e busco uma oportunidade de ingressar
     na área de tecnologia de forma profissional exercendo com segurança e confiança
     todos os meus aprendizados nas tecnologias HTML5, CSS3, JavaSript e GIT/GitHub.
-    além de estar estudando Bootstrap,Node.js e futuramente Vue.js.`;
+    além de estar estudando Bootstrap,Node.js e futuramente React.js`;
     mais.style.fontSize='1.2em';
     mais.style.marginTop="20px";
     botao.style.display='none';
@@ -39,7 +39,7 @@ function SobreMim(){
     let foto = document.createElement("img");
     foto.setAttribute("title","Huan Figuereido dev front-end");
     eu.appendChild(foto);
-    foto.src="eu.jpg";
+    foto.src="assets/img/eu.JPG";
     foto.style.marginTop="20px;"
     foto.style.filter="drop-shadow:5px 5px 10px;";
     //adicionando animação
